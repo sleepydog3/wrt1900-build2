75ac1e873d89e6dab5d40dbe1661b4472682b747
sleepydog3 <89539863+sleepydog3@users.noreply.github.com>
2021-08-25 19:50:53 +0200

    Initial commit

---
+++ scripts/github_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#################################################################
+# (C) 2021 By Eliminater74 For OpenWRT
+# Updated: 20210822
+#
+#
+#################################################################
+#
+# To run Simple GITHUB commands and prepare Dir
+#
+#################################################################
+cd openwrt/bin
+D="$(date +"%Y.%m.%d-%H%M")"
+echo "[+] Creating Directory: $D"
+mkdir $D
+
+
+
+
+exit 0

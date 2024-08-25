554e22744ae2e6cfc36dc6fb99591c2ad39faf11
Your Name <you@example.com>
2024-08-25 17:19:10 +0200

    reconfigured from basic & reorganized repo

---
+++ bpir3-scripts/custom-feeds.sh
@@ -0,0 +1,6 @@
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' $BUILDROOT/feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >> $BUILDROOT/feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> $BUILDROOT/feeds.conf.default

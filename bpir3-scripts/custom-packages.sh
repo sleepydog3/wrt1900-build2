6c016b5123932c13709a8827437dc343ea9ffeed
Your Name <you@example.com>
2024-09-06 13:40:35 +0200

    added some new themes

--- bpir3-scripts/custom-packages.sh
+++ bpir3-scripts/custom-packages.sh
@@ -8,3 +8,8 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git
 mv luci-theme-argon $BUILDROOT/package
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
 mv luci-app-argon-config $BUILDROOT/package
+
+# some more themes
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial

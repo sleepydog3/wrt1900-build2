4d9cedcd236634a6470a3a1260cd21f1ac528bde
Your Name <you@example.com>
2024-12-12 13:05:41 +0100

    fix9

--- bpir3-scripts/custom-config.sh
+++ bpir3-scripts/custom-config.sh
@@ -60,8 +60,8 @@ sed -i 's/redirect_https\s*0/redirect_https   1/g' $BUILDROOT/package/network/se
 
 # Apply MediaTek OpenWrt files and patches
 echo ">> Apply MediaTek OpenWrt files and patches"
-cp -af $BUILDROOT/feeds/mtk_openwrt_feed/master/files/* $BUILDROOT
-ls -l $BUILDROOT
+cp -af $BUILDROOT/feeds/mtk_openwrt_feed/master/files/* $BUILDROOT/
+ls -l $BUILDROOT/
 echo ">> >>"
 for file in $(find $BUILDROOT/feeds/mtk_openwrt_feed/master/patches-base -name "*.patch" | sort); do 
     echo ">> >> patching $file"

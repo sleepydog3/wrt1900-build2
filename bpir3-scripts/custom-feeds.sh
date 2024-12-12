b710a39392609c021b698551644d9f93ce427900
Your Name <you@example.com>
2024-12-12 10:14:25 +0100

    fix5

--- bpir3-scripts/custom-feeds.sh
+++ bpir3-scripts/custom-feeds.sh
@@ -6,4 +6,5 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> $BUILDROOT/feeds.conf.default
 
 # Add MediaTek OpenWrt feed
-echo "src-git mtk_openwrt_feed https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds" >> feeds.conf.default
+echo "Adding MediaTek OpenWrt feed"
+echo "src-git mtk_openwrt_feed https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds" >> $BUILDROOT/feeds.conf.default
